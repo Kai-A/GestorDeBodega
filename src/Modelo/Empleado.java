a199be648b92f2dab17cddcb35926d51413d798b
Kai-A <Kaikia01234@gmail.com>
2019-09-19 18:11:52 -0500

    Modelos

---
+++ src/Modelo/Empleado.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public class Empleado extends Persona {
+    public  void modificar(){}
+
+    public void listar(){}
+
+    public void buscar(){}
+
+    public void eliminar(){}
+}
